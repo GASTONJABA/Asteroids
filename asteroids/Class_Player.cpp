75f7d75ee3e8ac3ffa2bd43fd60092ab0b871471
GASTONJABA <GASGHIGO@GMAIL.COM>
2025-09-05 19:30:47 -0300

    con clase

---
+++ asteroids/Class_Player.cpp
@@ -0,0 +1,24 @@
+#include "Class_Player.h"
+
+Class_Player::Class_Player(Vector2 position)
+{
+
+}
+
+void Class_Player::DrawPlayer()
+{
+}
+
+void Class_Player::Shoot()
+{
+}
+
+void Class_Player::Update()
+{
+}
+
+void Class_Player::Input()
+{
+}
+
+
